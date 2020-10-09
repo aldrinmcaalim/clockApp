a2f95f93039e4591ea2640d62ed2225ba49762a6
Aldrin <aldrinmcaalim@gmail.com>
2020-10-08 20:36:46 -0400

    Setting the time

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const hourHand = document.querySelector('.hour-hand');
 const minsHand = document.querySelector('min-hand');
 const secondHand = document.querySelector('.second-hand');
+
+
+function setTime() {
+    const time = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+}
