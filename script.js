95132eb7878bb166e846baeb1e803de2bf8ff1dc
Aldrin Caalim <58124048+aldrinmcaalim@users.noreply.github.com>
2020-10-09 22:26:59 -0400

    Working on hour hand

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function setDate() {
 
 	const hour = now.getHours();
 	const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+	hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
 
 }
