14eb046c00cc11e447a68c9a74e277bbda31d73c
Aldrin Caalim <58124048+aldrinmcaalim@users.noreply.github.com>
2020-10-09 21:50:13 -0400

    Working on the clock hands

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const hourHand = document.querySelector('.hour-hand');
 const minsHand = document.querySelector('min-hand');
 const secondHand = document.querySelector('.second-hand');
 
-
 function setTime() {
-    const time = new Date();
+	const time = new Date();
 
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
+	const seconds = now.getSeconds();
+	const secondsDegrees = seconds / 60 * 360 + 90;
+	secondHand.getElementsByClassName.transform = `rotate(${secondsDegrees}deg)`;
 }
