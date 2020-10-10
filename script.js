c53bee2d1631882be7e0a820627e09b93045b194
Aldrin Caalim <58124048+aldrinmcaalim@users.noreply.github.com>
2020-10-09 22:17:54 -0400

    Working on minute hand

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const hourHand = document.querySelector('.hour-hand');
-const minsHand = document.querySelector('min-hand');
 const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
-function setTime() {
-	const time = new Date();
+function setDate() {
+	const now = new Date();
 
 	const seconds = now.getSeconds();
-	const secondsDegrees = seconds / 60 * 360 + 90;
-	secondHand.getElementsByClassName.transform = `rotate(${secondsDegrees}deg)`;
+	const secondsDegrees = ((seconds / 60) * 360) + 90;
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+	const mins = now.getMinutes();
+	const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+
 }
